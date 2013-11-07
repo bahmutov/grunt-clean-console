dee42641cb8c9303ebd96b92f79d6e3d085abc4a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-11-07 13:24:26 -0500

    initial

---
+++ README.md
@@ -0,0 +1,70 @@
+# grunt-nice-package
+
+> Opinionated package.json validator
+
+[![NPM info][nodei.co]](https://npmjs.org/package/grunt-nice-package)
+
+[![Build status][ci-image]][ci-url]
+[![dependencies][dependencies-image]][dependencies-url]
+[![endorse][endorse-image]][endorse-url]
+
+## Install
+
+```shell
+npm install grunt-nice-package --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-nice-package');
+grunt.initConfig({
+  'nice-package': {
+    all: {
+      options: {
+        version: function (value) {
+          // strict version number validation
+          return (/\d{1,2}\.\d{1,2}\.\d{1,2}/).test(value);
+        }
+      }
+    }
+  }
+})
+```
+
+Please return `true` if the check passes from custom validation functions.
+
+After the property validators pass, package.json is further checked using
+[package-json-validator](http://package-json-validator.com/) by Nick Sullivan.
+It will check the required properties, and provide suggestions for
+the recommended ones.
+
+## Options
+
+You can define a validation function for any property of the *package.json*, by default
+the validation will check:
+
+* name
+* version
+* description
+* license or licenses
+* keywords (array of strings)
+
+For complete list see
+[nice_package.js](https://github.com/bahmutov/grunt-nice-package/blob/master/tasks/nice_package.js#L24) for details.
+
+## Small print
+
+Author: Gleb Bahmutov &copy; 2013
+
+License: MIT - do anything with the code, but don't blame me if it does not work.
+
+Support: if you find any problems with this module, email / tweet / open issue on Github
+
+[ci-image]: https://travis-ci.org/bahmutov/grunt-nice-package.png?branch=master
+[ci-url]: https://travis-ci.org/bahmutov/grunt-nice-package
+[nodei.co]: https://nodei.co/npm/grunt-nice-package.png?downloads=true
+[dependencies-image]: https://david-dm.org/bahmutov/grunt-nice-package.png
+[dependencies-url]: https://david-dm.org/bahmutov/grunt-nice-package
+[endorse-image]: https://api.coderwall.com/bahmutov/endorsecount.png
+[endorse-url]: https://coderwall.com/bahmutov
---
+++ tasks/clean-console.js
@@ -0,0 +1,122 @@
+/*
+ * grunt-nice-package
+ * https://github.com/bahmutov/grunt-nice-package
+ *
+ * Copyright (c) 2013 Gleb Bahmutov
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var PJV = require('package-json-validator').PJV;
+var check = require('check-types');
+
+function unary(fn) {
+  return function (first) {
+    return fn(first);
+  };
+}
+
+module.exports = function(grunt) {
+
+  var is = function (type, name, value) {
+    if (!check[type](value)) {
+      grunt.log.error('expected', name, 'to be', type, 'not', value);
+      return false;
+    }
+    return true;
+  };
+
+  var defaultValidators = {
+    name: is.bind(null, 'string', 'name'),
+    version: is.bind(null, 'string', 'version'),
+    description: is.bind(null, 'string', 'description'),
+
+    keywords: function (values) {
+      if (!check.array(values)) {
+        grunt.log.error('expected keywords to be an Array');
+        return false;
+      }
+
+      return values.every(function (keyword) {
+        if (!check.string(keyword)) {
+          grunt.log.error('every keyword should be a string, found', keyword);
+          return false;
+        }
+        return true;
+      });
+    },
+    author: function (value) {
+      if (!check.object(value) &&
+        !check.string(value)) {
+        grunt.log.error('invalid author value', value);
+        return false;
+      }
+      return true;
+    },
+    repository: function (value) {
+      if (!check.object(value)) {
+        grunt.log.error('expected repository to be an object, not', value);
+        return false;
+      }
+      if (!check.string(value.type)) {
+        grunt.log.error('expected repository type to be a string, not', value.type);
+        return false;
+      }
+      if (!check.string(value.url)) {
+        grunt.log.error('expected repository url to be a string, not', value.url);
+        return false;
+      }
+      return true;
+    }
+  };
+
+  grunt.registerMultiTask('nice-package', 'Opinionated package.json validator', function() {
+    // Merge custom validation functions with default ones
+    var options = this.options(defaultValidators);
+
+    var pkg = grunt.file.readJSON('package.json');
+
+    var every = Object.keys(options).every(function (key) {
+      grunt.verbose.writeln('checking property', key);
+
+      var property = pkg[key];
+      if (!property) {
+        grunt.log.error('package.json missing', key);
+        return false;
+      }
+      if (typeof options[key] === 'function') {
+        if (!options[key](property)) {
+          grunt.log.error('failed check for property', key);
+          return false;
+        }
+      }
+
+      return true;
+    });
+
+    if (!every) {
+      // return false;
+    }
+
+    // advanced checking
+    if (!check.string(pkg.license) &&
+      !check.array(pkg.licenses)) {
+      grunt.log.error('missing license information');
+      return false;
+    }
+
+    var result = PJV.validate(JSON.stringify(pkg, null, 2));
+    if (!result.valid) {
+      grunt.log.subhead("Errors:");
+      result.errors.forEach(unary(grunt.log.error));
+    }
+    if (check.array(result.warnings) &&
+      result.warnings.length) {
+      grunt.log.subhead("Warnings:");
+      result.warnings.forEach(unary(grunt.log.warn));
+    }
+    return !!result.valid;
+  });
+
+};
